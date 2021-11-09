5054de8cd515910ac2915995c854a04e55b8775f
NicGiles <n.r.giles1@gmail.com>
2021-11-09 19:56:39 +0000

    struggling to parse info

--- functions/scrape.js
+++ functions/scrape.js
@@ -1,3 +1,8 @@
+const HTMLParser = require('node-html-parser')
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+
 const scrapePersons = async () => {
   // import launchChrome and newPage from the browser.js file in the same directory
   const { launchChrome } = require("./browser");
@@ -16,15 +21,95 @@ const scrapePersons = async () => {
     await page.goto(url, {
       waitUntil: "networkidle0", // wait till all network requests has been processed
     });
+
+await page.click('#_evidon-accept-button');
+
+    await page.type('input[type=email]', 'candidate+7@wonderbill.com');
+    await page.type('input[type=password]', 'M/Cw?7w!G/6-');
+    await page.click('button[type=submit]');
+
+    await page.waitForNavigation();
   } catch(e) {
     console.error("Unable to visit " + url, e);
     await exitChrome(); // close chrome on error
     return; // exiting the function
   }
 
+//const clickBtn = async (nth = 0) => {
+ // try {
+  //  await page.evaluate(() => {
+   //   const btnSelector = ".btn";
+    //  const btn = document.querySelectorAll(btnSelector)[nth];
+     // btn.focus();
+    // btn.click();
+  // });
+ // } catch(e) {
+ //   console.error("Unable to click button", e);
+ // }
+//};
+
+//const text = HTMLParser.parse('${url}')
+
+//var name = url.getElementById('_2u4DS'),
+
+//htmlContent = name.innerHTML,
+// htmlContent = "Some <span class="foo">sample</span> text."
+
+//textContent = name.textContent;
+// textContent = "Some sample text."
+
+//console.log(htmlContent)
+//console.log(textContent)
+
+//console.log(url.span._2u4DS)
+
+//<span class="_2u4DS">Donec bibendum</span>
+
+//console.log(text)
+
+
+const NameDiv = '${url}'._1MwNx
+
+//const extractPersons = async () => {
+  //try {
+   // const persons = await page.evaluate(() => {
+     // let persons = [];
+     // const personsElem = document.querySelectorAll($NameDiv);
+     // const personsElemLen = personsElem.length;
+
+      //for (let i = 0; i < personsElemLen; i++) {
+        //try {
+         // const personElem = personsElem[i];
+
+         // const photo = personElem.querySelector("{$NameDev}._2u4DS").src;
+console.log(NameDiv)
+          //const name = personElem.querySelector("h3").innerText;
+         // const email = personElem.querySelector(".email").innerText;
+        //const phone = personElem.querySelector(".phone").innerText;
+          
+ //         persons.push({ photo });
+   //     } catch (e) {}
+     // }
+
+      //return persons;
+   // });
+
  
 
-  await exitChrome(); // close chrome
+    // do anything with persons
+
+   // console.log(persons.length, "persons", persons);
+ // } catch(e) {
+   // console.error("Unable to extract persons data", e);
+ // }
+//};
+//extractPersons();
+
+//clickBtn(0); // clicks the 1st occurence of the ".btn" selector
+
+ 
+
+// await exitChrome(); // close chrome
 };
 
 module.exports = scrapePersons;
